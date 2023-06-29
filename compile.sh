d0081dbaf69e384ff6403f3588bc1642547ce3fe
Andreas Kugel <andreas.kugel@web.de>
2023-06-29 18:01:48 +0200

    update

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-prez --theme akTheme --highlight-theme akHighlight --title zkm  --serve 
+#prez --theme akTheme --highlight-theme akHighlight --title zkm  --serve 
+# don't serve
+./node_modules/.bin/prez --theme akTheme --highlight-theme akHighlight --title zkm  
+
+
